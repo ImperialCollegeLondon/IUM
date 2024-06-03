540b0f92c1693fe187e5a599c4658d2a1c4f300d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-06-03 09:21:56 +0100

    make the repo compile; flag old stuff

--- IUM.lean
+++ IUM.lean
@@ -1,3 +1,3 @@
--- This module serves as the root of the `IUM` library.
--- Import modules here that should be built as part of the library.
-import «IUM».Basic
+import IUM.«2023».IntegerGame
+import IUM.«2023».RationalGameAlgebra
+import IUM.«2023».RationalGameOrder
---
+++ IUM/Old/README.md
@@ -0,0 +1,1 @@
+This is all stuff which was autoported from Lean 3 and will need work to get going again.
